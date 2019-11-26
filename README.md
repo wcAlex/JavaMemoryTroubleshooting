ccf8589f8d17352925e380468a35c77bfe1f3ef9
chiwang-sf <chi.wang@salesforce.com>
2019-11-25 16:43:40 -0800

    OOM files

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # JavaMemoryTroubleshooting
 Examples and tips of solving problems relating to memory in Java
 
-## Memory leak sample 1 ()
+## Memory leak sample 1 (Simple Memory Leak)
 
 setup test application and generate load.
 
@@ -20,5 +20,16 @@ or running as webservice:
 Open Profiler, check out heap old generation, you will find memory is leaking. The root cause is missing equal function for cache id comparison, so the cache keeps growing.
 Check out code: <https://github.com/wcAlex/JavaMemoryTroubleshooting/blob/master/memory_leaks1/src/main/java/com/monotonic/klassified/resources/PostingCache.java#L42>
 
-## Memory leak sample 2
+## Memory leak sample 2 (out of memory)
 
+Start `MemoryHeavyStatisticsService` as a backend api server, which will load entire file as one go, thus cause a memory leak.
+https://github.com/wcAlex/JavaMemoryTroubleshooting/tree/master/outofmemory.
+
+Note that the project is set max heap size to 50MB and heap dump will be generated automatically when OOM happens, please check out pom file for details.
+
+check out ./run.sh to post a file upload request to cause OOM. The test csv file is from https://www.gov.uk/government/statistical-data-sets/uk-house-price-index-data-downloads-april-2016, any 50M+ csv file will trigger OOM.
+```bash
+curl -X POST --data-binary @$HOME/Downloads/UK-HPI-full-file-2016-04.csv 'http://localhost:9800'
+```
+
+Proper way of reading files is at `MemoryLightStatisticsService`, suggest to consume file line by line.
--- outofmemory/src/main/java/MemoryHeavyStatisticsService.java
+++ outofmemory/src/main/java/MemoryHeavyStatisticsService.java
@@ -1,3 +1,4 @@
+import com.opencsv.CSVParser;
 import com.opencsv.CSVReader;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
@@ -7,13 +8,11 @@ import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MemoryHeavyStatisticsService extends HttpServlet
 {
@@ -25,7 +24,7 @@ public class MemoryHeavyStatisticsService extends HttpServlet
         final ServletHandler servletHandler = new ServletHandler();
         servletHandler.addServletWithMapping(MemoryHeavyStatisticsService.class, "/");
 
-        final Server server = new Server(9000);
+        final Server server = new Server(9800);
         server.setHandler(servletHandler);
         server.dumpStdErr();
         server.start();
@@ -34,24 +33,33 @@ public class MemoryHeavyStatisticsService extends HttpServlet
 
     @Override
     protected void doPost(
-        final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException
-    {
-        final ServletInputStream inputStream = req.getInputStream();
-        final CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream));
-        final Iterator<String[]> iterator = csvReader.iterator();
-        iterator.next();
+        final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        // load all lines into memory
+        final List<String> lines = readLines(req);
+        final List<Integer> salesVolumes = parseSalesVolumes(lines);
+        final double mean = mean(salesVolumes);
+        printResponse(resp, mean);
+    }
 
+    // set VM options to: -Xmx50M -XX:+HeapDumpOnOutOfMemoryError to have a OOM heap dump.
+    private List<Integer> parseSalesVolumes(final List<String> lines) throws IOException {
+        final CSVParser csvParser = new CSVParser();
         final List<Integer> salesVolumes = new ArrayList<>();
-        while (iterator.hasNext())
+        for (String line : lines)
         {
-            final String[] row = iterator.next();
+            final String[] row = csvParser.parseLine(line);
             final String salesVolumeCell = row[SALES_VOLUME_COL];
             final int salesVolume = salesVolumeCell.isEmpty() ? 0 : Integer.parseInt(salesVolumeCell);
             salesVolumes.add(salesVolume);
         }
+        return salesVolumes;
+    }
 
-        final double mean = mean(salesVolumes);
-        printResponse(resp, mean);
+    private List<String> readLines(final HttpServletRequest req) throws IOException
+    {
+        final ServletInputStream inputStream = req.getInputStream();
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        return reader.lines().skip(1).collect(Collectors.toList());
     }
 
     private void printResponse(final HttpServletResponse resp, final double mean) throws IOException
